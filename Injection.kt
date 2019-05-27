2da415cbf0d9dceb1f4a4196a272f1180c139456
Fiqri Hafzain <33598887+syntaxxxxx@users.noreply.github.com>
2019-05-27 07:33:35 +0700

    Create Injection.kt
    fixing

---
+++ Injection.kt
@@ -0,0 +1,41 @@
+object Injection {
+
+    private fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        val loggingInterceptor = HttpLoggingInterceptor()
+        loggingInterceptor.level = if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor.Level.BODY
+        } else {
+            HttpLoggingInterceptor.Level.NONE
+        }
+        return loggingInterceptor
+    }
+
+    private fun provideOkHttpClient(): OkHttpClient {
+        val okHttpClient = OkHttpClient.Builder().addInterceptor(provideLoggingInterceptor())
+            .connectTimeout(10, TimeUnit.SECONDS)
+            .writeTimeout(10, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .build()
+        return okHttpClient
+    }
+
+    private fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient())
+            .build()
+    }
+    
+    fun provideApiService(): ApiService = provideRetrofit().create(ApiService::class.java)
+    
+    interface BelanjaApi {
+
+    @FormUrlEncoded
+    @POST("example.php")
+    fun login(
+        @Field("email") email: String,
+        @Field("password") password: String
+    ): Call<Response>
+  }
+}
